386bb9ea55fb0a9cd5c1e082b1fbfffe27901549
Aparna <aparnaacharya12@gmail.com>
2020-07-26 20:16:09 +0530

    Commit using EGIT Plugin

--- src/test/java/com/w2a/rough/LoginTest.java
+++ src/test/java/com/w2a/rough/LoginTest.java
@@ -12,9 +12,7 @@ import com.w2a.pages.crm.accounts.CreateAccountPage;
 
 
 public class LoginTest extends Page {
-//This is a rough package
-	//testing CI
-	//again check
+//Using EGIT plugin
 public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		HomePage home=new HomePage();
